6ffdf982e08dec143f428d64327958c7c568e724
fguopp <33630915+fguopp@users.noreply.github.com>
2017-11-28 17:55:03 -0600

    Add files via upload

--- hour_analysis_of_call_log.py
+++ hour_analysis_of_call_log.py
@@ -97,6 +97,40 @@ test['call_date'] = test['call_date'].map(lambda x: datetime.strptime(x, "%Y-%m-
 merged_data = pd.merge(st, test, on = 'call_date')
 merged_data = merged_data.fillna(0)
 
+
+import numpy as np
+import pandas as pd
+import time
+import re
+from datetime import datetime
+import sys
+import os
+import re
+import calendar
+
+
+test = pd.read_csv("Desktop/direct mail analysis/new/new_call_volumn_inputs.csv")
+# INDICATOR_COLS = test.columns
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+import matplotlib.dates as mdates
+import datetime as dt
+
+df_ohlc = test[['call_nums', 'weekday', 'days_from_last_1_maildate']]
+# ax = df_ohlc.boxplot(column='call_nums', by='weekday', grid=False)
+ax = df_ohlc.boxplot(column='call_nums', by='days_from_last_1_maildate', grid=False)
+
+
+df_ohlc = test[['call_nums', 'weekday', 'days_from_last_3_maildate']]
+# ax = df_ohlc.boxplot(column='call_nums', by='weekday', grid=False)
+ax = df_ohlc.boxplot(column='call_nums', by='days_from_last_3_maildate', grid=False)
+plt.show()
+
+df_ohlc.groupby(['weekday']).describe().to_csv("Desktop/direct mail analysis/new/by_weekday_data_describe.csv")
+df_ohlc.groupby(['days_from_last_1_maildate']).describe().to_csv("Desktop/direct mail analysis/new/by_lastmaildate_data_describe.csv")
+
+
 # from scipy import stats
 # b = list()
 # for i in range(24):
@@ -105,57 +139,57 @@ merged_data = merged_data.fillna(0)
 #     slope, intercept, r_value, p_value, std_err = stats.mstats.linregress(x,y)
 #     b.append([slope, intercept, r_value*r_value, p_value, std_err])
 
-
-b = list()
-from sklearn import linear_model
-for i in range(24):
-    y = merged_data[i].values.reshape((173, 1))
-    x = merged_data[['numbers_y', 'weekday']].values.reshape((173, 2))
-    regr = linear_model.LinearRegression()
-    # regr = LinearRegression()
-    regr.fit(x, y)
-    b.append([regr.coef_, regr.intercept_, regr.score(x,y)])
-
-bb = list()
-for i in range(len(b)):
-    bb.append([b[i][0][0], b[i][1][0], b[i][2]])
-
-
-combined_regression = pd.DataFrame(bb, columns = ['coefficent', 'intercept', 'r-square'])
-combined_regression.to_csv("Desktop/direct mail analysis/untitled/combined_regression_orig.csv")
-
-
-
-from sklearn import linear_model
-from scipy import stats
-import numpy as np
-
-
-class LinearRegression(linear_model.LinearRegression):
-    """
-    LinearRegression class after sklearn's, but calculate t-statistics
-    and p-values for model coefficients (betas).
-    Additional attributes available after .fit()
-    are `t` and `p` which are of the shape (y.shape[1], X.shape[1])
-    which is (n_features, n_coefs)
-    This class sets the intercept to 0 by default, since usually we include it
-    in X.
-    """
-    def __init__(self, fit_intercept=True, normalize=False, copy_X=True, n_jobs=1):
-        self.fit_intercept = fit_intercept
-        self.normalize = normalize
-        self.copy_X = copy_X
-        self.n_jobs = n_jobs
-    def fit(self, X, y, n_jobs=1):
-        self = super(LinearRegression, self).fit(X, y, n_jobs)
-        sse = np.sum((self.predict(X) - y) ** 2, axis=0) / float(X.shape[0] - X.shape[1])
-        se = np.array([
-            np.sqrt(np.diagonal(sse[i] * np.linalg.inv(np.dot(X.T, X))))
-                                                    for i in range(sse.shape[0])
-                    ])
-        self.t = self.coef_ / se
-        self.p = 2 * (1 - stats.t.cdf(np.abs(self.t), y.shape[0] - X.shape[1]))
-        return self
-
-
-
+#
+#b = list()
+#from sklearn import linear_model
+#for i in range(24):
+#    y = merged_data[i].values.reshape((173, 1))
+#    x = merged_data[['numbers_y', 'weekday']].values.reshape((173, 2))
+#    regr = linear_model.LinearRegression()
+#    # regr = LinearRegression()
+#    regr.fit(x, y)
+#    b.append([regr.coef_, regr.intercept_, regr.score(x,y)])
+#
+#bb = list()
+#for i in range(len(b)):
+#    bb.append([b[i][0][0], b[i][1][0], b[i][2]])
+#
+#
+#combined_regression = pd.DataFrame(bb, columns = ['coefficent', 'intercept', 'r-square'])
+#combined_regression.to_csv("Desktop/direct mail analysis/untitled/combined_regression_orig.csv")
+#
+#
+#
+#from sklearn import linear_model
+#from scipy import stats
+#import numpy as np
+#
+#
+#class LinearRegression(linear_model.LinearRegression):
+#    """
+#    LinearRegression class after sklearn's, but calculate t-statistics
+#    and p-values for model coefficients (betas).
+#    Additional attributes available after .fit()
+#    are `t` and `p` which are of the shape (y.shape[1], X.shape[1])
+#    which is (n_features, n_coefs)
+#    This class sets the intercept to 0 by default, since usually we include it
+#    in X.
+#    """
+#    def __init__(self, fit_intercept=True, normalize=False, copy_X=True, n_jobs=1):
+#        self.fit_intercept = fit_intercept
+#        self.normalize = normalize
+#        self.copy_X = copy_X
+#        self.n_jobs = n_jobs
+#    def fit(self, X, y, n_jobs=1):
+#        self = super(LinearRegression, self).fit(X, y, n_jobs)
+#        sse = np.sum((self.predict(X) - y) ** 2, axis=0) / float(X.shape[0] - X.shape[1])
+#        se = np.array([
+#            np.sqrt(np.diagonal(sse[i] * np.linalg.inv(np.dot(X.T, X))))
+#                                                    for i in range(sse.shape[0])
+#                    ])
+#        self.t = self.coef_ / se
+#        self.p = 2 * (1 - stats.t.cdf(np.abs(self.t), y.shape[0] - X.shape[1]))
+#        return self
+#
+#
+#
